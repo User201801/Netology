49f4277ab599acdbb7c26a696ffe9ca700126bb4
Александр <ref201801@gmail.com>
2026-06-22 23:11:53 +0300

    Задача 1. Арифметические функции

---
+++ Part_1/ConsoleApplication/Exercise_6_1/Main.cpp
@@ -0,0 +1,64 @@
+﻿//Задача 1. Арифметические функции
+//Описание
+//Дана программа :
+//
+//int main(int argc, char** argv)
+//{
+//	int a = 5, b = 10;
+//
+//	int s = sum(a, b);
+//	int dif = diff(a, b);
+//	int mult = multiplication(a, b);
+//	double div = division(a, b);
+//
+//	std::cout << a << " + " << b << " = " << s << std::endl;
+//	std::cout << a << " - " << b << " = " << dif << std::endl;
+//	std::cout << a << " * " << b << " = " << mult << std::endl;
+//	std::cout << a << " / " << b << " = " << div << std::endl;
+//
+//	return 0;
+//}
+//В ней используются функции sum, diff, multiplication, division.Ваша задача — написать эти функции, чтобы результат работы программы выглядел следующим образом :
+//
+//5 + 10 = 15
+//5 - 10 = -5
+//5 * 10 = 50
+//5 / 10 = 0.5
+
+#include <iostream>
+
+// Функция сложения
+int sum(int a, int b) {
+    return a + b;
+}
+
+// Функция вычитания
+int diff(int a, int b) {
+    return a - b;
+}
+
+// Функция умножения
+int multiplication(int a, int b) {
+    return a * b;
+}
+
+// Функция деления
+double division(int a, int b) {
+    return static_cast<double>(a) / b;
+}
+
+int main() {
+    int a = 5, b = 10;
+
+    int s = sum(a, b);
+    int dif = diff(a, b);
+    int mult = multiplication(a, b);
+    double div = division(a, b);
+
+    std::cout << a << " + " << b << " = " << s << std::endl;
+    std::cout << a << " - " << b << " = " << dif << std::endl;
+    std::cout << a << " * " << b << " = " << mult << std::endl;
+    std::cout << a << " / " << b << " = " << div << std::endl;
+
+    return 0;
+}
