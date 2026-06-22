22cfb5ae4e4eeaf0958425f74a8d855ff1900761
Александр <ref201801@gmail.com>
2026-06-22 22:32:20 +0300

    Задача 2. Сумма цифр числа

---
+++ Part_1/ConsoleApplication/Exercise_4_2/Main.cpp
@@ -0,0 +1,44 @@
+﻿//Задача 2. Сумма цифр числа
+//Описание
+//Напишите программу, которая запрашивает у пользователя целое число и выводит на экран сумму всех его цифр.
+//
+//Примеры работы программы
+//Введите целое число :
+//7345
+//Сумма цифр : 19
+//Введите целое число :
+//1234
+//Сумма цифр : 10
+
+#include <iostream>
+#include <cmath>
+#include <windows.h>
+
+// Функция для подсчета суммы цифр
+int sumOfDigits(int number) {
+    int sum = 0;
+    number = std::abs(number);  // Работаем с положительным числом
+
+    while (number > 0) {
+        sum += number % 10;
+        number /= 10;
+    }
+
+    return sum;
+}
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int number = 0;
+
+    std::cout << "Введите целое число: ";
+    std::cin >> number;
+
+    int result = sumOfDigits(number);
+
+    std::cout << "Сумма цифр: " << result << std::endl;
+
+    return 0;
+}
