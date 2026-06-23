8db924434c1c07a1baf379f2a1fc20d5afed5fdc
Александр <ref201801@gmail.com>
2026-06-23 23:04:26 +0300

    Задача 1. Ввод массива с консоли

--- Part_1/ConsoleApplication/Exercise_9_3/Main.cpp
+++ Part_1/ConsoleApplication/Exercise_9_3/Main.cpp
@@ -39,6 +39,7 @@ void reverse(int* arr, int size) {
 }
 
 int main() {
+    setlocale(LC_ALL, "Russian");
     // Создаём массив
     int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
     int size = sizeof(arr) / sizeof(arr[0]);
