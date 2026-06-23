9f0877269daba29125198dfe974ab410a8ceada1
Александр <ref201801@gmail.com>
2026-06-23 22:13:34 +0300

    Задача 1. Считающая функция

---
+++ Part_1/ConsoleApplication/Exercise_8_1/Main.cpp
@@ -0,0 +1,50 @@
+﻿//Задача 1. Считающая функция
+//Описание
+//Нужно написать функцию, которая отслеживает, сколько раз она была вызвана за время работы программы.
+//
+//Посмотрите на этот код :
+//
+//int main(int argc, char** argv)
+//{
+//    for (int i = 0; i < 15; i++)
+//    {
+//        counting_function();
+//    }
+//}
+//Ваша задача — написать функцию counting_function так, чтобы программа имела вывод на консоль, как показано ниже.
+//
+//Пример работы программы
+//Количество вызовов функции counting_function() : 1
+//Количество вызовов функции counting_function() : 2
+//Количество вызовов функции counting_function() : 3
+//Количество вызовов функции counting_function() : 4
+//Количество вызовов функции counting_function() : 5
+//Количество вызовов функции counting_function() : 6
+//Количество вызовов функции counting_function() : 7
+//Количество вызовов функции counting_function() : 8
+//Количество вызовов функции counting_function() : 9
+//Количество вызовов функции counting_function() : 10
+//Количество вызовов функции counting_function() : 11
+//Количество вызовов функции counting_function() : 12
+//Количество вызовов функции counting_function() : 13
+//Количество вызовов функции counting_function() : 14
+//Количество вызовов функции counting_function() : 15
+
+#include <iostream>
+
+int call_count = 0;  // Глобальная переменная
+
+void counting_function()
+{
+    call_count++;
+    std::cout << "Количество вызовов функции counting_function(): " << call_count << std::endl;
+}
+
+int main(int argc, char** argv)
+{
+    setlocale(LC_ALL, "Russian");
+    for (int i = 0; i < 15; i++)
+    {
+        counting_function();
+    }
+}
